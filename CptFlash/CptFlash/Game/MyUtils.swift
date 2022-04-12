fb050dd8006b8d3b254daac950cb3a7c90765cdc
James Murphy <20044578@mail.wit.ie>
2022-04-12 15:08:12 +0100

    Prototype-4
    Added two other enemies with better AI and improved boss AI. Added power-ups for better weapons and invincibility.

---
+++ CptFlash/CptFlash/Game/MyUtils.swift
@@ -0,0 +1,20 @@
+//
+//  MyUtils.swift
+//  CptFlash
+//
+//  Created by James Murphy on 14/03/2022.
+//
+
+import Foundation
+import CoreGraphics
+
+extension CGFloat {
+  static func random() -> CGFloat {
+    return CGFloat(Float(arc4random()) / Float(UInt32.max))
+  }
+
+  static func random(min: CGFloat, max: CGFloat) -> CGFloat {
+    assert(min < max)
+    return CGFloat.random() * (max - min) + min
+  }
+}
